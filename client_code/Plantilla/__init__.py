7f279aca655dcfffcb33ef0678b031f8a193cc8e
naielvarona@gmail.com <naielvarona@gmail.com>
2024-01-11 19:18:29 +0000

    Created Responsables, Main and Plantilla; Edited settings; Deleted Form1

--- client_code/Plantilla/__init__.py
+++ client_code/Plantilla/__init__.py
@@ -1,16 +1,20 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import PlantillaTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
-class Form1(Form1Template):
+class Plantilla(PlantillaTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Main')
